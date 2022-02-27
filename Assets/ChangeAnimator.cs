129804d1fd6de6bd6530ba515a9cf887039a2ff1
Poppy0302 <75597118+Poppy0302@users.noreply.github.com>
2022-02-27 16:39:52 +0700

    first commit

---
+++ Assets/ChangeAnimator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void getAnimatorDancing() {
+        this.GetComponent<Animator>().Play("Dancing Twerk");
+    }
+    public void getAnimatorRumba() {
+        this.GetComponent<Animator>().Play("Rumba Dancing");
+    }
+}
